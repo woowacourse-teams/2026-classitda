9b810f082767990e2eb0ff7883fe24de30bf6745
MODUGGAGI <kiroro0814@naver.com>
2026-08-04 18:19:53 +0900

    init: 백엔드 프로젝트 초기 세팅
    - Java 21 및 Gradle Wrapper 구성
    - Spring Boot Web MVC 기본 의존성 설정
    - 애플리케이션 및 기본 테스트 추가

---
+++ BE/src/test/java/com/classitda/ClassitdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.classitda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassitdaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
