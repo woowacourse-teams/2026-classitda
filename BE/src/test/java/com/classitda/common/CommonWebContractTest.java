2964bb46a04d322cf09d2e809c4c29f5a0b5c0b4
MODUGGAGI <kiroro0814@naver.com>
2026-08-10 15:13:14 +0900

    test: 약관 조회 테스트와 계층별 테스트 원칙 반영

--- BE/src/test/java/com/classitda/common/CommonWebContractTest.java
+++ BE/src/test/java/com/classitda/common/CommonWebContractTest.java
@@ -17,8 +17,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@AutoConfigureRestTestClient
 @Import({ApiVersionConfig.class, GlobalExceptionHandler.class, CommonWebContractTest.TestController.class})
+@AutoConfigureRestTestClient
 @WebMvcTest(controllers = CommonWebContractTest.TestController.class)
 class CommonWebContractTest {
 
