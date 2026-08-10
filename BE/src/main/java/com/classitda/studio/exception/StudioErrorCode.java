2659f81a84ca3a3d391e688c84d6f946bc0f01d4
sinequanonn <dleck288@gmail.com>
2026-08-10 21:21:57 +0900

    feat: 룸 에러 코드 추가

--- BE/src/main/java/com/classitda/studio/exception/StudioErrorCode.java
+++ BE/src/main/java/com/classitda/studio/exception/StudioErrorCode.java
@@ -6,9 +6,14 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum StudioErrorCode implements ErrorCode {
+    // STUDIO
     INVALID_OPERATING_TIME("STUDIO-001", "운영 종료 시간은 시작 시간보다 늦어야 합니다.", HttpStatus.BAD_REQUEST),
     NOT_FOUND("STUDIO-002", "시설을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
-    NOT_OWNER("STUDIO-003", "해당 시설의 대표 강사가 아닙니다.", HttpStatus.FORBIDDEN);
+    NOT_OWNER("STUDIO-003", "해당 시설의 대표 강사가 아닙니다.", HttpStatus.FORBIDDEN),
+
+    // ROOM
+    ROOM_NOT_FOUND("ROOM-001", "룸을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    ROOM_NAME_DUPLICATED("ROOM-002", "이미 사용 중인 룸 이름입니다.", HttpStatus.CONFLICT);
 
     private final String code;
     private final String message;
