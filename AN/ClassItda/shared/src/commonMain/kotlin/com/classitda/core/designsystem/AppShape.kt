a917c887c918ce566c1621e1dee9ef5f46c22cf7
todays-sun-day <178suna@gmail.com>
2026-08-11 15:39:41 +0900

    feat: 공통 UI 컴포넌트 구현
    - AppShape 파일 생성
    - AppTopBar 생성
    - 공용으로 쓰는 Student 용 PrimaryButton 생성

---
+++ AN/ClassItda/shared/src/commonMain/kotlin/com/classitda/core/designsystem/AppShape.kt
@@ -0,0 +1,10 @@
+package com.classitda.core.designsystem
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.unit.dp
+
+object AppShape {
+    val Pill = RoundedCornerShape(100.dp)
+    val Card = RoundedCornerShape(10.dp)
+    val Button = RoundedCornerShape(10.dp)
+}
