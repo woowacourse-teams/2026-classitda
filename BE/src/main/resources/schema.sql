37005be95646f5e86902c199a8fa2be5c3ddefab
sinequanonn <dleck288@gmail.com>
2026-08-12 21:30:13 +0900

    remove: data.sql을 schema.sql로 위임

--- BE/src/main/resources/schema.sql
+++ BE/src/main/resources/schema.sql
@@ -314,3 +314,22 @@ CREATE TABLE notice
     CONSTRAINT fk_notice_author FOREIGN KEY (author_membership_id) REFERENCES studio_membership (id)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
+
+-- 권한 코드. PermissionCode enum과 1:1로 대응한다.
+-- enum에 상수를 추가하면 이 파일에도 함께 추가해야 한다.
+INSERT INTO permission (code, created_at)
+VALUES ('STUDIO_UPDATE', NOW(6)),
+       ('POLICY_MANAGE', NOW(6)),
+       ('ROOM_MANAGE', NOW(6)),
+       ('ROLE_MANAGE', NOW(6)),
+       ('MEMBER_READ', NOW(6)),
+       ('MEMBER_INVITE', NOW(6)),
+       ('MEMBER_MANAGE', NOW(6)),
+       ('CLASS_TEMPLATE_MANAGE', NOW(6)),
+       ('CLASS_SESSION_MANAGE_OWN', NOW(6)),
+       ('CLASS_SESSION_MANAGE_ALL', NOW(6)),
+       ('RESERVATION_READ', NOW(6)),
+       ('RESERVATION_MANAGE', NOW(6)),
+       ('PASS_PRODUCT_MANAGE', NOW(6)),
+       ('PASS_MEMBER_MANAGE', NOW(6)),
+       ('NOTICE_MANAGE', NOW(6));
