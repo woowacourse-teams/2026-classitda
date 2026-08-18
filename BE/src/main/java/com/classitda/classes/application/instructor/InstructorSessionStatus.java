5012fa227e1a291fbe2c64b0f4f86e442582adba
MODUGGAGI <kiroro0814@naver.com>
2026-08-18 11:27:37 +0900

    feat: ClassSession 강사용 수업 상태 판정 구현

---
+++ BE/src/main/java/com/classitda/classes/application/instructor/InstructorSessionStatus.java
@@ -0,0 +1,9 @@
+package com.classitda.classes.application.instructor;
+
+public enum InstructorSessionStatus {
+    SCHEDULED_OPEN,
+    SCHEDULED_CLOSED,
+    IN_PROGRESS,
+    COMPLETED,
+    CANCELED
+}
