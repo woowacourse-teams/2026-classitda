99f9ae0d18b76d04deff02ff6fb2886992b07834
BaekCCI <bsy8470@naver.com>
2026-08-18 11:29:19 +0900

    feat: 홈 화면에서 사용하는 도메인 모델 정의

--- AN/ClassItda/shared/src/commonMain/kotlin/com/classitda/domain/model/home/Reservation.kt
+++ AN/ClassItda/shared/src/commonMain/kotlin/com/classitda/domain/model/home/Reservation.kt
@@ -2,11 +2,19 @@ package com.classitda.domain.model.home
 
 import kotlinx.datetime.LocalDateTime
 
-data class UpcomingReservation(
+data class PendingReservation(
     val id: String,
     val className: String,
     val instructorName: String,
     val startAt: LocalDateTime,
     val endAt: LocalDateTime,
     val memo: String,
+    val pass: ReservationPassSummary,
+)
+
+data class ReservationPassSummary(
+    val passName: String,
+    val totalRemainingCount: Int,
+    val reservableCount: Int,
+    val cancellableCount: Int,
 )
