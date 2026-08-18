59ee2edf9aa677215cf113ac097073877a891a6e
MODUGGAGI <kiroro0814@naver.com>
2026-08-18 17:25:11 +0900

    chore: Swagger API 버전 기본값 설정

--- BE/src/main/java/com/classitda/common/config/SwaggerConfig.java
+++ BE/src/main/java/com/classitda/common/config/SwaggerConfig.java
@@ -6,9 +6,13 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springdoc.core.customizers.OpenApiCustomizer;
 
 @Configuration
 public class SwaggerConfig {
+
+    private static final String API_VERSION_HEADER = "X-API-Version";
+    private static final String DEFAULT_API_VERSION = "1";
 
     @Bean
     public OpenAPI openAPI() {
@@ -24,5 +28,17 @@ public class SwaggerConfig {
                                 .type(SecurityScheme.Type.HTTP)
                                 .scheme("bearer")
                                 .bearerFormat("JWT")));
+    }
+
+    @Bean
+    public OpenApiCustomizer apiVersionHeaderDefaultCustomizer() {
+        return openApi -> openApi.getPaths().values().stream()
+                .flatMap(pathItem -> pathItem.readOperations().stream())
+                .filter(operation -> operation.getParameters() != null)
+                .flatMap(operation -> operation.getParameters().stream())
+                .filter(parameter -> API_VERSION_HEADER.equals(parameter.getName()))
+                .filter(parameter -> "header".equals(parameter.getIn()))
+                .filter(parameter -> parameter.getSchema() != null)
+                .forEach(parameter -> parameter.getSchema().setDefault(DEFAULT_API_VERSION));
     }
 }
