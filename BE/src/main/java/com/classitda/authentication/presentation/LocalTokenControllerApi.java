452902674c4ad9234e2b95a31e1399dd0e41664e
MODUGGAGI <kiroro0814@naver.com>
2026-08-20 13:15:44 +0900

    chore: 개발 배포 환경에서 로컬 토큰 발급 허용

--- BE/src/main/java/com/classitda/authentication/presentation/LocalTokenControllerApi.java
+++ BE/src/main/java/com/classitda/authentication/presentation/LocalTokenControllerApi.java
@@ -9,14 +9,14 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
-// TODO : 로컬 개발 끝나면 삭제 예정
-@Tag(name = "로컬 인증", description = "로컬 Swagger 테스트 전용 인증 API")
+// TODO: 실제 운영 전환 전에 LocalTokenController와 함께 제거한다.
+@Tag(name = "개발용 인증", description = "개발 환경의 Swagger 테스트용 인증 API")
 public interface LocalTokenControllerApi {
 
     @Operation(
-            summary = "로컬 테스트용 로그인 토큰 발급",
+            summary = "개발용 로그인 토큰 발급",
             description = "일반 인증 절차를 생략하고 입력한 회원 ID로 Access Token과 Refresh Token을 발급합니다. "
-                    + "local 프로필에서만 사용할 수 있으며 기본 테스트 회원 ID는 1입니다."
+                    + "개발 중인 배포 환경에서만 사용하며 기본 테스트 회원 ID는 1입니다."
     )
     @ApiResponse(
             responseCode = "200",
