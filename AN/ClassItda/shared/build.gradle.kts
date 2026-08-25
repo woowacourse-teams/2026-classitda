3dfd101ba0c57ac99520ec3c41afc7f876575589
Yun Ji <20220723@duksung.ac.kr>
2026-08-25 20:08:32 +0900

    feat: 시설 이미지 카메라와 갤러리 선택 기반을 추가

--- AN/ClassItda/shared/build.gradle.kts
+++ AN/ClassItda/shared/build.gradle.kts
@@ -50,6 +50,7 @@ kotlin {
 
     sourceSets {
         androidMain.dependencies {
+            implementation(libs.androidx.activity.compose)
             implementation(libs.compose.uiToolingPreview)
             implementation(libs.compose.uiTooling)
             implementation(libs.ktor.client.android)
