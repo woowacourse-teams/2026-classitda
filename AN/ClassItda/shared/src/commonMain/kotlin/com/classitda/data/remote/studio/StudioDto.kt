fed02e8d8e5581ae97654800207cd98f9788c7ef
todays-sun-day <178suna@gmail.com>
2026-08-25 14:36:34 +0900

    feat: add remote studio api repository

---
+++ AN/ClassItda/shared/src/commonMain/kotlin/com/classitda/data/remote/studio/StudioDto.kt
@@ -0,0 +1,15 @@
+package com.classitda.data.remote.studio
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+internal data class StudioResponseDto(
+    val id: Long,
+    val name: String,
+    val address: String,
+    val phoneNumber: String,
+    val openTime: String? = null,
+    val closeTime: String? = null,
+    val imageUrl: String? = null,
+    val description: String? = null,
+)
