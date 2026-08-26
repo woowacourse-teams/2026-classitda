df1e4c115a69ae1e701f8f800af4da2097e2b9db
todays-sun-day <178suna@gmail.com>
2026-08-26 16:15:21 +0900

    회원 예약 등록 요청에 JSON 타입 지정

--- AN/ClassItda/shared/src/commonMain/kotlin/com/classitda/data/remote/instructor/member/InstructorMemberApi.kt
+++ AN/ClassItda/shared/src/commonMain/kotlin/com/classitda/data/remote/instructor/member/InstructorMemberApi.kt
@@ -7,6 +7,8 @@ import io.ktor.client.request.get
 import io.ktor.client.request.parameter
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
 
 internal class InstructorMemberApi(
     private val client: HttpClient,
@@ -29,6 +31,7 @@ internal class InstructorMemberApi(
     ) {
         client
             .post("api/studios/$studioId/instructor/class-sessions/$sessionId/enrollments") {
+                contentType(ContentType.Application.Json)
                 setBody(InstructorEnrollmentCreateRequestDto(membershipId))
             }
     }
