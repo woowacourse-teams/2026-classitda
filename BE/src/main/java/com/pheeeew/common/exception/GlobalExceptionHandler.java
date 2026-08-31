feede023fea297ac21357f0cd702c5d18e288b51
MODUGGAGI <kiroro0814@naver.com>
2026-08-31 16:47:52 +0900

    feat(be): 공통 API 예외 응답 처리 구현

---
+++ BE/src/main/java/com/pheeeew/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,57 @@
+package com.pheeeew.common.exception;
+
+import static com.pheeeew.common.exception.CommonErrorCode.ENDPOINT_NOT_FOUND;
+import static com.pheeeew.common.exception.CommonErrorCode.INTERNAL_SERVER_ERROR;
+import static com.pheeeew.common.exception.CommonErrorCode.INVALID_REQUEST;
+
+import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(PheeeewException.class)
+    public ResponseEntity<ErrorResponse> handlePheeeewException(PheeeewException exception) {
+        return toResponseEntity(exception.getErrorCode());
+    }
+
+    @ExceptionHandler({
+            MethodArgumentNotValidException.class,
+            BindException.class,
+            HandlerMethodValidationException.class,
+            ConstraintViolationException.class,
+            HttpMessageNotReadableException.class
+    })
+    public ResponseEntity<ErrorResponse> handleInvalidRequest(Exception exception) {
+        return toResponseEntity(INVALID_REQUEST);
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNoResourceFound(NoResourceFoundException exception) {
+        return toResponseEntity(ENDPOINT_NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleUnexpectedException(Exception exception) {
+        log.error(
+                "처리되지 않은 예외가 발생했습니다. exceptionType={}",
+                exception.getClass().getName(),
+                exception
+        );
+        return toResponseEntity(INTERNAL_SERVER_ERROR);
+    }
+
+    private ResponseEntity<ErrorResponse> toResponseEntity(ErrorCode errorCode) {
+        return ResponseEntity.status(errorCode.getStatus())
+                .body(ErrorResponse.from(errorCode));
+    }
+}
