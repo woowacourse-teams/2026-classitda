51fe61699ec4e1a2221a6004f6125957abb47b74
MODUGGAGI <kiroro0814@naver.com>
2026-08-31 12:13:31 +0900

    chore(be): initialize Spring Boot project

---
+++ BE/src/main/java/com/pheeeew/PheeeewApplication.java
@@ -0,0 +1,12 @@
+package com.pheeeew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PheeeewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PheeeewApplication.class, args);
+    }
+}
