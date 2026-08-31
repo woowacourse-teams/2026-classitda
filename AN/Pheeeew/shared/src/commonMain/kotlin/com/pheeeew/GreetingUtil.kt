41af829a7af59c1e330f98251692ab5de4fca9c7
todays-sun-day <178suna@gmail.com>
2026-08-31 15:07:08 +0900

    feat: CMP 프로젝트 초기 설정

---
+++ AN/Pheeeew/shared/src/commonMain/kotlin/com/pheeeew/GreetingUtil.kt
@@ -0,0 +1,4 @@
+package com.pheeeew
+
+fun sayHello(to: String): String =
+    "Hello, $to!"
---
+++ AN/Pheeeew/shared/src/commonMain/kotlin/com/pheeeew/Platform.kt
@@ -0,0 +1,7 @@
+package com.pheeeew
+
+interface Platform {
+    val name: String
+}
+
+expect fun getPlatform(): Platform
