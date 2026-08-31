f2f6aaac9fb10de43b86d6069f200388eb95d126
MODUGGAGI <kiroro0814@naver.com>
2026-08-31 15:47:25 +0900

    feat(be): JPA 감사 정보 공통 관리 기반 추가

---
+++ BE/src/main/java/com/pheeeew/common/domain/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.pheeeew.common.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.Instant;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private Instant updatedAt;
+}
