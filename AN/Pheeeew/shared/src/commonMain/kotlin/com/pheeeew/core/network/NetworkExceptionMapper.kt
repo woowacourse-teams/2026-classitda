f8bcd5459e5a40b59d1f171c72dc1205b391ea2d
todays-sun-day <178suna@gmail.com>
2026-09-01 13:37:39 +0900

    기능: 코루틴 취소 예외 재전파 처리

--- AN/Pheeeew/shared/src/commonMain/kotlin/com/pheeeew/core/network/NetworkExceptionMapper.kt
+++ AN/Pheeeew/shared/src/commonMain/kotlin/com/pheeeew/core/network/NetworkExceptionMapper.kt
@@ -3,10 +3,15 @@ package com.pheeeew.core.network
 import com.pheeeew.domain.exception.ApiException
 import io.ktor.client.network.sockets.ConnectTimeoutException
 import io.ktor.client.network.sockets.SocketTimeoutException
+import kotlinx.coroutines.CancellationException
 import kotlinx.io.IOException
 
 fun Throwable.toApiException(): ApiException =
     when (this) {
+        is CancellationException -> {
+            throw this
+        }
+
         is ConnectTimeoutException,
         is SocketTimeoutException,
         is IOException,
